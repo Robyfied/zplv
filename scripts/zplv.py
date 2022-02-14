e9b5782c86daea66f1d594a68c5af82f458edf93
Robert Musceleanu <robert.musceleanu@cnva.eu>
2022-02-14 21:54:26 +0200

    finished program

--- scripts/zplv.py
+++ scripts/zplv.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 
 ####
-#### CA SA MODIFICI PENTRU ANUL URMATOR SCHIMBA LINIILE 39, 40, 43 SI 56
+#### CA SA MODIFICI PENTRU ANUL URMATOR SCHIMBA LINIILE 
 ####
 
 class Scraper:
@@ -32,13 +32,12 @@ class Scraper:
         df.columns = ["Judet", "Localitate", "Populatie", "Incidenta", "Cazuri Confirmate"]
         return df.Incidenta[df.Localitate == "MUNICIPIUL GALAŢI"].values[0]
 
-class ProgressBar:
+class Progress:
     def school_days(self, _date: dt.date):
         _days_off = [dt.date(2021, 10, 5), dt.date(2021, 11, 30), dt.date(2021, 12, 1), dt.date(2021, 12, 23), dt.date(2021, 12, 24), dt.date(2021, 12, 27), dt.date(2021, 12, 28), dt.date(2021, 12, 29), dt.date(2021, 12, 30), dt.date(2021, 12, 31), dt.date(2022, 1, 24),  dt.date(2022, 4, 15), dt.date(2022, 4, 18), dt.date(2022, 4, 19), dt.date(2022, 4, 20), dt.date(2022, 4, 21), dt.date(2022, 4, 22), dt.date(2022, 4, 23), dt.date(2022, 4, 25), dt.date(2022, 4, 26), dt.date(2022, 4, 27), dt.date(2022, 4, 28), dt.date(2022, 4, 29), dt.date(2022, 6, 1)]
         _curr_date = dt.date(2021, 9, 13)   # first day of school
         school_days = 0
-        i = 0
-        while i < 270:   # 270 = days from first day of school to last day of school
+        while _curr_date <= dt.date(2022, 6, 10):   # last day of school
             school_days += 1
             if _curr_date.weekday() > 4:
                 school_days -= 1
@@ -48,13 +47,33 @@ class ProgressBar:
                         school_days -= 1
             if _curr_date == _date:
                 return school_days
-            _curr_date += dt.timedelta(days = 1)
-            i += 1
+            _curr_date += dt.timedelta(days=1)
+
+    def days_until_vacation(self):
+        _vacation_days = [dt.date(2021, 12, 24), dt.date(2022, 4, 15), dt.date(2022, 6, 10)]
+        for day in _vacation_days:
+            if dt.date.today() <= day:
+                return (day - dt.date.today()).days
+
+    def schooldays_until_vacation(self):
+        _vacation_days = [dt.date(2021, 12, 24), dt.date(2022, 4, 15), dt.date(2022, 6, 10)]
+        for day in _vacation_days:
+            if dt.date.today() <= day:
+                return (self.school_days(day) - self.school_days(dt.date.today()))
+
     def progress_bar(self, _date: dt.date):
         perc = round(self.school_days(_date) / 171 * 100, 2)   # 171 = total school days
-        return perc
-        #TODO: actual ascii progress bar ▰▰▰▰▰▰▰▰▰▰▰▱▱▱▱▱▱▱▱▱ (increases every 5%)
+        bar = ""
+        i = 5
+        while i <= 100:
+            if i <= perc:
+                bar += "▰"
+            else:
+                bar += "▱"
+            i += 5
+        return (bar + " " + str(perc) + "%")
         
+    
 
 
 
