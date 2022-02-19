a4727ea8d34c56a3b7ecb92983f903183cb09a8e
Robert Musceleanu <robert.musceleanu@cnva.eu>
2022-02-20 00:22:18 +0200

    error handling changes

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ from scripts.zplv import Scraper
 from scripts.zplv import Progress
 import datetime as dt
 from pandas import errors as PandasErrors
+from requests import exceptions as RequestsErrors
 ####
 ####  CA SA MODIFICI PENTRU ANUL URMATOR MODIFICA IN scripts/zplv.py LINIILE 35, 36, 38, 51, 57 SI 63
 ####
 
 output = open("output.txt", "w", encoding="utf-8")
+
 try:
     inc_azi = Scraper().get_incidenta(dt.date.today())
     inc_ieri = Scraper().get_incidenta(dt.date.today() - dt.timedelta(days=1))
 except PandasErrors.EmptyDataError: 
-    output.write("!!! S-A BUSIT SCRAPERUL")
+    output.write("!!! S-A BUSIT SCRAPERUL. DATA DOWNLOAD FAILED.")
+    print("!!! S-A BUSIT SCRAPERUL. DATA DOWNLOAD FAILED.")
     inc_azi, inc_ieri = 0, 0
+except RequestsErrors.ConnectionError:
+    output.write("!!! NO INTERNET. DATA DOWNLOAD FAILED.")
+    print("!!! NO INTERNET. DATA DOWNLOAD FAILED.")
+    inc_azi, inc_ieri = 0, 0
+if inc_azi is None:
+    output.write("!!! INCA NU A APARUT INCIDENTA AZI")
+    print("!!! INCA NU A APARUT INCIDENTA AZI")
+    inc_azi, inc_ieri = 0, 0
+
 
 if dt.date.today() > dt.date(2022, 6, 10):
     output.write("\nE vacanta de vara!\n Asteapta sa inceapa anul scolar ca sa updatez programul si stai la curent cu http://github.com/Robyfied/zplv")
-elif inc_azi is None:
-    output.write("INCA NU A APARUT INCIDENTA AZI (sau s-a stricat scraperul)")
 else:
     dif = round(inc_azi - inc_ieri, 2)
     if dif < 0:
